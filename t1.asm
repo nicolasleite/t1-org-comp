44f9ab604026e865575f4a7a97f4734fd73cfc39
nicolasleite <nicolasleite96@gmail.com>
2015-05-14 15:03:42 -0300

    new entry

--- t1.asm
+++ t1.asm
@@ -1,114 +1,84 @@
-#.data
-#	type:	.asciiz "tipo: "
-#	number:	.asciiz "numero: "
-#	enter:	.asciiz "\n"
-#	bin:	.byte 'B'
-#	oct:	.byte 'O'
-#	dec:	.byte 'D'
-#	hex:	.byte 'H'
-
-#.text
-#
-#	main:
-#		la $a0, type		#print("tipo: ");
-#		li $v0, 4
-#		syscall
-#		li $v0, 5 
-#		syscall
-
-#	convertBinary:
-
-#	convertOctal:
-
-#	convertDecimal:
-
-#	convertHexa:
-
-
-#	**************************************************************
-
 .data
 	msg1: .asciiz "Please insert value (A > 0) : "
-	msg2: .asciiz "Please insert the number system B you want to 
-	convert to (2<=B<=10): "
-	#Above sting must be in one line
+	msg2: .asciiz "Please insert the number system B you want to convert to (2<=B<=10):"
 	msg3: .asciiz "\nResult : "
+
 .text
-	.globl main
-	main:
-		addi $s0,$zero,2
-		addi $s1,$zero,10
-	getA:
+.globl main #make main global
 
-		li $v0,4
-		la $a0,msg1
-		syscall
-		li $v0,5
-		syscall
-		blt $v0,$zero,getA
+#$zero ==0
 
-		move $t0,$v0
-	getB:
+main:
+	addi $s0,$zero,2 #s0 = 2
+	addi $s1,$zero,10 #s1 = 10
 
-		li $v0,4
-		la $a0,msg2
-		syscall
-		li $v0,5
-		syscall
-		blt $v0,$s0,getB
-		bgt $v0,$s1,getB
+getA:
+	li $v0,4	#print
+	la $a0,msg1 #msg
+	syscall		#do
+	li $v0,5	#read N
+	syscall		#do
+	blt $v0,$zero,getA #if N < 0, goto getA
+	move $t0,$v0 #t0 = N
 
-		add $t1,$zero,$v0
+getB:
+	li $v0,4	#print
+	la $a0,msg2	#msg
+	syscall		#do
+	li $v0,5	#read B
+	syscall		#do
+	blt $v0,$s0,getB #if B < 2, goto getB
+	bgt $v0,$s1,getB #if B > 10, goto getB
+	add $t1,$zero,$v0 # t1 = B
+	li $v0,4	#print
+	la $a0,msg3	#msg
+	syscall		#do
+	add $a0,$zero,$t0 # a0 = N
+	add $a1,$zero,$t1 # a1 = B
+	jal convert #call
+	li $v0,10	#exit
+	syscall		#do
 
-		li $v0,4
-		la $a0,msg3
-		syscall
+convert:
+	#a0=A
+	#a1=B
+	###
+	addi $sp,$sp,-16 # 4 words
+	sw $s3,12($sp) # push1
+	sw $s0,8($sp) # push2 
+	sw $s1,4($sp) # push3 
+	sw $ra,0($sp) # push4
+	###
+	###
+	add $s0,$zero,$a0 #s0 = N
+	add $s1,$zero,$a1 #s1 = B
+	###
+	beqz $s0,end #if==0. goto end
+	div $t4,$s0,$s1 #t4= N/B
+	rem $t3,$s0,$s1 #t3= N%B
+	
+	add $sp,$sp,-4 # 1 word 
+	sw $t3,0($sp) #push5
+	
+	add $a0,$zero,$t4 #a0 = t4 
+	###
+	add $a1,$zero,$s1 #a1 = B garantia
+	###
+	###
+	addi $s3,$s3,1	#done só uma vez... useless counter (add)?
+	###
+	jal convert        #call convert
+	
+end:
+	lw $ra,0($sp) 
+	lw $s1,4($sp) 
+	lw $s0,8($sp) 
+	lw $s3,12($sp) 
+	beqz $s3,done #if==0. goto done. para ignorar o last 0 pushado
+	lw $a0,16($sp) #load o t3 no a0 (pro v0 fazer os roles)
+	li $v0,1	#print integer
+	syscall		#do
 
-		add $a0,$zero,$t0
-		add $a1,$zero,$t1
-
-		jal convert
-
-		li $v0,10
-		syscall
-
-	convert:
-		#a0=A
-		#a1=B
-
-		addi $sp,$sp,-16
-
-		sw $s3,12($sp) #counter,used to know
-		#how many times we will pop from stack
-		sw $s0,8($sp) #A
-		sw $s1,4($sp) #B
-		sw $ra,0($sp)
-
-		add $s0,$zero,$a0
-		add $s1,$zero,$a1
-
-		beqz $s0,end
-
-		div $t4,$s0,$s1 #t4=A/B
-		rem $t3,$s0,$s1 #t3=A%B
-		add $sp,$sp,-4
-		sw $t3,0($sp) #save t3
-
-		add $a0,$zero,$t4 #pass A/B
-		add $a1,$zero,$s1 #pass B
-		addi $s3,$s3,1
-		jal convert        #call convert
-
-	end:
-
-		lw $ra,0($sp)
-		lw $s1,4($sp)
-		lw $s0,8($sp)
-		lw $s3,12($sp)
-		beqz $s3,done
-		lw $a0,16($sp)
-		li $v0,1
-		syscall
-	done: 
-		addi $sp,$sp,20
-		jr $ra   #return
+done: 
+	addi $sp,$sp,20 # inc sp
+	jr $ra   #rts call
